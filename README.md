f9b995e28de7e0a8d2053bb9729e3a34fba0b778
Matthew Nanas <37747855+matthewnanas@users.noreply.github.com>
2025-04-16 23:58:00 -0400

    Create hello world attestation

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # eas-sandbox
 
 **Note:** This proof of concept uses the sepolia testnet. I highly recommend using a faucet to get test funds. Check out [Google&#39;s Faucet](https://cloud.google.com/application/web3/faucet/ethereum/sepolia)
+
+## Configuration
+
+Configure your .env file in the root directory to have the following:
+
+```
+PRIVATE_KEY="<Private key of sepolia wallet>"
+```
+
+## Running
+
+First install dependencies by running
+
+```
+$ yarn install
+```
+
+in the root directory.
+
+Then run
+
+```
+$ yarn run
+```
+
+To run the test script!
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { EAS, SchemaEncoder } from "@ethereum-attestation-service/eas-sdk";
+import { EAS, NO_EXPIRATION, SchemaEncoder } from "@ethereum-attestation-service/eas-sdk";
 import { ethers } from "ethers";
 import dotenv from 'dotenv';
 
@@ -18,6 +18,43 @@ async function main() {
 
     // Connect to EAS with the signer
     eas.connect(signer);
+
+    // Define schema
+    const schemaEncoder = new SchemaEncoder("string message");
+
+    // Encode data according to the schema
+    const encodedData = schemaEncoder.encodeData([
+        { name: "message", value: "hello world - DGC", type: "string" }
+    ]);
+
+    /**
+     * Notes from matt:
+     * 
+     * The encoded data must adhere to an existing schemaUID on EAS. Check out https://sepolia.easscan.org/schema/create
+     * to create a schema. If it already exists, you can use the schema ID returned. Note that you must have your
+     * wallet connected to use the tool.
+     */
+    const schemaUID = '0x3969bb076acfb992af54d51274c5c868641ca5344e1aacd0b1f5e4f80ac0822f'; 
+
+    console.log("Sending transaction to EAS... (give this a moment!)");
+    // Create the attestation
+    const transaction = await eas.attest({
+        schema: schemaUID,
+        data: {
+            recipient: ethers.ZeroAddress,
+            expirationTime: NO_EXPIRATION,
+            revocable: true,
+            data: encodedData,
+        },
+    });
+
+    const newAttestationUID = await transaction.wait();
+
+    console.log("Attestation created with UID:", newAttestationUID);
+    console.log("Transaction hash:", transaction.tx.hash);
+
+    console.log("Transaction URL:", `https://sepolia.etherscan.io/tx/${transaction.tx.hash}`);
+    console.log("Attestation URL:", `https://sepolia.easscan.org/attestation/view/${newAttestationUID}`);
 }
 
 main().catch((error: any) => {
