244ec612054f4d56921371aa9f56fd3bf5c2e5dc
Matthew Nanas <37747855+matthewnanas@users.noreply.github.com>
2025-04-22 22:28:41 -0400

    Finish workflow

--- src/workflows/workflow-qr.ts
+++ src/workflows/workflow-qr.ts
@@ -3,8 +3,24 @@
  * generates/signs an ethereum attestation
  */
 
+import { EAS, NO_EXPIRATION, SchemaEncoder } from '@ethereum-attestation-service/eas-sdk';
+import { ethers } from 'ethers';
 import { Jimp } from 'jimp'
 import jsQR from 'jsqr'
+import * as dotenv from "dotenv";
+
+dotenv.config();
+/**
+ * Schema Definition
+ * UID: 0x1893a6a17c2621355f1912a37fc3918057e41e8534b3fa56963ceafffdce49bf
+ * URL: https://sepolia.easscan.org/schema/view/0x1893a6a17c2621355f1912a37fc3918057e41e8534b3fa56963ceafffdce49bf
+ */
+interface SchemaDefinition {
+  lat: string;
+  long: string;
+  id: string;
+  timestamp: string;
+}
 
 /**
  * Read an image from disk and decodes QR code
@@ -36,12 +52,67 @@ async function decodeQR(imagePath: string): Promise<string> {
   }
 }
 
+// Generate a random UUID v4
+function uuidv4() {
+  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+    (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+  );
+}
+
+async function createAttestation(qrData: string): Promise<void> {
+  const eas = new EAS("0xC2679fBD37d54388Ce493F1DB75320D236e1815e")
+  const provider = ethers.getDefaultProvider('sepolia')
+
+  // Create a signer
+  const privateKey = process.env.PRIVATE_KEY
+  if (!privateKey) throw new Error("set PRIVATE_KEY in .env")
+  const signer = new ethers.Wallet(privateKey, provider)
+
+  eas.connect(signer)
+
+  const time = new Date()
+
+  const schemaEncoder = new SchemaEncoder("string lat, string long, string id, string timestamp")
+  const encodedData = schemaEncoder.encodeData([
+    { name: "lat", value: JSON.parse(qrData)[0].toString(), type: "string" },
+    { name: "long", value: JSON.parse(qrData)[1].toString(), type: "string" },
+    { name: "id", value: uuidv4(), type: "string" },
+    { name: "timestamp", value: time.toLocaleDateString(), type: "string" }
+  ])
+
+  const schemaUID = "0x1893a6a17c2621355f1912a37fc3918057e41e8534b3fa56963ceafffdce49bf"
+
+  console.log(
+    `Creating attestation with schema UID: ${schemaUID}, data: ${encodedData}`
+  )
+  const transaction = await eas.attest({
+    schema: schemaUID,
+    data: {
+      recipient: signer.address,
+      expirationTime: NO_EXPIRATION,
+      revocable: true,
+      data: encodedData,
+    }
+  })
+
+  const newAttestationUID = await transaction.wait()
+
+  console.log("Attestation created with UID:", newAttestationUID);
+  console.log("Transaction hash:", transaction.receipt?.hash);
+
+  console.log("Transaction URL:", `https://sepolia.etherscan.io/tx/${transaction.receipt?.hash}`);
+  console.log("Attestation URL:", `https://sepolia.easscan.org/attestation/view/${newAttestationUID}`);
+}
+
 async function main() {
   const imagePath = 'src/assets/qrcode.png'
 
   try {
     const qrData = await decodeQR(imagePath)
     console.log('Decoded QR code data:', qrData)
+
+    await createAttestation(qrData)
+    console.log('Attestation created successfully!')
   } catch (error) {
     console.error(error)
   }
