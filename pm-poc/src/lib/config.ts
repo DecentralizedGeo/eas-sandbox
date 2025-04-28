c967b14881fb61a09a4cfd208662af36b7d666d8
AR <53452227+arrnd@users.noreply.github.com>
2025-04-28 17:12:09 -0400

    PM proof of concept

--- pm-poc/src/lib/config.ts
+++ pm-poc/src/lib/config.ts
@@ -1,5 +1,5 @@
 export const SEPOLIA_CHAIN_ID = '0xC2679fBD37d54388Ce493F1DB75320D236e1815e'
-export const SCHEMA_UID = "0x1893a6a17c2621355f1912a37fc3918057e41e8534b3fa56963ceafffdce49bf"
+export const SCHEMA_UID = "0x3bbefdae7c3c11b7b4f04d168a3672f46b7a620fec5ffa5e45a9539434ca87d0"
 
 export const EASContractAddress = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; // Sepolia v0.26
 export const EASSchemaRegistryAddress = "0x0a7E2Ff54e76B8E6659aedc9103FB21c038050D0"; // Sepolia Schema Registry v0.26
