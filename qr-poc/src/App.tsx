f70daa84b16e1a2f17facafe09ae712f7ee26aff
SethDocherty <sddocherty@gmail.com>
2025-04-29 10:11:03 -0400

    Refactor import statement to remove React import as it's not used
    I was getting the following error: `TypeScript errors (TS6133) indicating that certain variables or imports are declared but never used in your code.`
    
    Copilot recommended this change + the following comment:
    - Modern React versions often don't require import React explicitly for JSX

--- qr-poc/src/App.tsx
+++ qr-poc/src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
 import { NO_EXPIRATION } from "@ethereum-attestation-service/eas-sdk";
 import './App.css';
