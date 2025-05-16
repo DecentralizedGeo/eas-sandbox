1d96081bff31d879d7207bfcdfd8842ba63b6c58
Matthew Nanas <37747855+matthewnanas@users.noreply.github.com>
2025-05-15 23:41:18 -0400

    Secret POC

--- src/config.ts
+++ src/config.ts
@@ -5,5 +5,5 @@ export const PRIVATE_DATA_SCHEMA_UID = "0x20351f973fdec1478924c89dfa533d8f872def
 export const PRIVATE_DATA_SCHEMA_STRING = "bytes32 privateData"; // Schema string for the root attestation
 // Add other non-sensitive configuration constants here as needed
 export const SEPOLIA_CHAIN_ID = '0xC2679fBD37d54388Ce493F1DB75320D236e1815e'
-export const SCHEMA_UID = "0x5a50d5b3cd8ccf80dace112e584d19164eebd05a570d13c619f0d6f95f7e5c68"
+export const SCHEMA_UID = "0x5894dbce58b99f80288b8a9e06bc4763d2df445b4c9e9f6ff302ab66aabec0fe"
 
